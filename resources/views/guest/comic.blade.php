17c06c622095fedf10c7c8f49c03fedccb2b73fe
saragrigolin <sara.grigolin95@gmail.com>
2022-02-14 17:09:45 +0100

    second part of main

--- resources/views/guest/comic.blade.php
+++ resources/views/guest/comic.blade.php
@@ -43,5 +43,71 @@
                 </div>
             </div>
         </div>
+        <div class="comic-info-container">
+            <div class="container info w-55">
+                <div class="left-info">
+                    <div class="info-title">
+                        <span>Talent</span>
+                    </div>
+                    <div class="subinfo">
+                        <div class="info-subtitle">
+                            <span>Art by:</span>
+                        </div>
+                        <div class="info-desc">
+                            @foreach($comic['artists'] as $artist)
+                                @if ($loop->last)
+                                <span><a href="#">{{ $artist }}</a></span>
+                                @else 
+                                <span><a href="#">{{ $artist}}</a>,</span>
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
+                    <div class="subinfo">
+                        <div class="info-subtitle">
+                            <span>Written by:</span>
+                        </div>
+                        <div class="info-desc">
+                            @foreach($comic['writers'] as $writer)
+                                @if ($loop->last)
+                                <span><a href="#">{{ $writer }}</a></span>
+                                @else 
+                                <span><a href="#">{{ $writer}}</a>,</span>
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <div class="right-info">
+                    <div class="info-title">
+                        <span>Specs</span>
+                    </div>
+                    <div class="subinfo">
+                        <div class="info-subtitle">
+                            <span>Series:</span>
+                        </div>
+                        <div class="info-desc">
+                            <span class="upper"><a href="#">action comics</a></span>
+                        </div>
+                    </div>
+                    <div class="subinfo">
+                        <div class="info-subtitle">
+                            <span>U.S. Price:</span>
+                        </div>
+                        <div class="info-desc">
+                            <span class="upper">{{ $comic['price'] }}</span>
+                        </div>
+                    </div>
+                    <div class="subinfo">
+                        <div class="info-subtitle">
+                            <span>On Sale Date:</span>
+                        </div>
+                        <div class="info-desc">
+                            <span>{{ date_format(date_create($comic['sale_date']),'M d Y') }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 @endsection
