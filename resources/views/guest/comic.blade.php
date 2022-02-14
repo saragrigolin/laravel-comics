5fbc0fd31f45ba1d6d813084e8cabfabb6a6bd74
saragrigolin <sara.grigolin95@gmail.com>
2022-02-14 15:39:34 +0100

    first part of main

--- resources/views/guest/comic.blade.php
+++ resources/views/guest/comic.blade.php
@@ -4,20 +4,44 @@
   {{$nomePagina}}
 @endsection
 
-{{-- @section('css')
-  <link rel="stylesheet" href="{{ asset('css/comic.css') }}">
-@endsection --}}
-
 @section('content')
 {{-- @dd($comic) --}}
-    <main>
+    <main class="comic">
         <div class="blue-bar"></div>
-        <div class="container w-60">
+        <div class="container w-55">
             <div class="comic-title">
                 <img src="{{ $comic['thumb'] }}" alt="{{$comic['title'] }}">
+                <div class="blue-title book">comic book</div>
+                <div class="blue-title gallery">view gallery</div>
             </div>
-            <h3>{{$comic['title']}}</h3>
-            <img src="{{ $comic['thumb'] }}" alt="{{$comic['title'] }}">
+            <div class="comic-book-container">
+                <div class="desc-container">
+                    <h3 class="book-title">{{$comic['title']}}</h3>
+                    <div class="available-container">
+                        <div class="left">
+                            <div class="price">
+                                <span class="span-price">U.S. Price: </span><span>{{$comic['price']}}</span>
+                            </div>
+                            <div class="available">
+                                <span class="span-available">available</span>
+                            </div>
+                        </div>
+                        <div class="right check-availability">
+                            <div class="available-text">
+                                <span>Check Availability</span>
+                                <i class="fa-solid fa-sort-down"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-desc">
+                        <p>{{$comic['description']}}</p>
+                    </div>
+                </div>
+                <div class="adv-container">
+                    <span>advertisement</span>
+                    <img src="{{asset('img/adv.jpg')}}" alt="">
+                </div>
+            </div>
         </div>
     </main>
 @endsection
